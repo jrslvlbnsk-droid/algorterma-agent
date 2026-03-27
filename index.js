90decdfe2094e407c00dcab479ee3b036587b708
jrslvlbnsk-droid <jrslvlbnsk@gmail.com>
2026-03-27 13:34:47 +0100

    FIX GEMINI MODEL

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const bot = new TelegramBot(process.env.TELEGRAM_TOKEN, {
 });
 
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+const model = genAI.getGenerativeModel({ model: 'gemini-2.0-flash' });
 
 const GITHUB_TOKEN = process.env.GITHUB_TOKEN;
 const GITHUB_USER = 'jrslvlbnsk-droid';
